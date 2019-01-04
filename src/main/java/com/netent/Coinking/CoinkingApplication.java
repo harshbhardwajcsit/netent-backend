bc0dcafb380b90b65047bdd5a25b265a1b48c508
Harsh Bhardwaj <hbhardwaj@ptc.com>
2019-01-04 18:54:10 +0530

    test basic API

--- src/main/java/com/netent/Coinking/CoinkingApplication.java
+++ src/main/java/com/netent/Coinking/CoinkingApplication.java
@@ -1,9 +1,16 @@
 package com.netent.Coinking;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.netent.*"})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class CoinkingApplication {
 
 	public static void main(String[] args) {
