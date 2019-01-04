36ef48ca591689c0bcb51e585de164b20f9db59b
Harsh Bhardwaj <hbhardwaj@ptc.com>
2019-01-04 18:58:23 +0530

    test basic API

--- src/main/java/com/netent/Coinking/resources/CoinKingResource.java
+++ src/main/java/com/netent/Coinking/resources/CoinKingResource.java
@@ -4,13 +4,13 @@ import com.netent.Coinking.service.CoinkingService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/coin")
-@Api(value = "/api/coin", description = "API service for coin king game")
 @ApiResponses(value = {
         @ApiResponse(code = 200, message = "Success"),
         @ApiResponse(code = 400, message = "Invalid Input"),
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
         @ApiResponse(code = 500, message = "Internal Server Error")})
 public class CoinKingResource {
 
+    @Autowired
     private CoinkingService coinkingService;
 
     @GetMapping("/result")
--- src/main/java/com/netent/Coinking/service/impl/CoinKingServiceImpl.java
+++ src/main/java/com/netent/Coinking/service/impl/CoinKingServiceImpl.java
@@ -1,7 +1,9 @@
 package com.netent.Coinking.service.impl;
 
 import com.netent.Coinking.service.CoinkingService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CoinKingServiceImpl implements CoinkingService {
     @Override
     public String fetchResult() {
