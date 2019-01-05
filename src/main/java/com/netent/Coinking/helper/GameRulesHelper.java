7708e3799d63bc78f234c912e1d4a3715a3bbb06
Harsh Bhardwaj <hbhardwaj@ptc.com>
2019-01-05 14:44:14 +0530

    Game API ready

---
+++ src/main/java/com/netent/Coinking/helper/GameRulesHelper.java
@@ -0,0 +1,14 @@
+package com.netent.Coinking.helper;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+public class GameRulesHelper {
+    public static final Map<Integer, String> rules = ImmutableMap.<Integer, String>builder()
+            .put(1, "BONUS")
+            .put(2, "WIN")
+            .put(3, "LOST")
+            .build();
+
+}
--- src/main/java/com/netent/Coinking/service/impl/CoinKingServiceImpl.java
+++ src/main/java/com/netent/Coinking/service/impl/CoinKingServiceImpl.java
@@ -1,12 +1,18 @@
 package com.netent.Coinking.service.impl;
 
+import com.netent.Coinking.helper.GameRulesHelper;
 import com.netent.Coinking.service.CoinkingService;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class CoinKingServiceImpl implements CoinkingService {
     @Override
     public String fetchResult() {
-        return "API is working";
+        final int[] RATES = {1, 2, 2, 2, 3, 3, 3, 3, 3, 3};
+        Random random = new Random();
+        int result = RATES[random.nextInt(RATES.length)];
+        return GameRulesHelper.rules.get(result);
     }
 }
