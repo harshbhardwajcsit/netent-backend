25874d7d26e08d5f751f39c16410958b45256651
Harsh Bhardwaj <hbhardwaj@ptc.com>
2019-01-08 00:26:11 +0530

    added rules

--- src/main/java/com/netent/Coinking/helper/GameRulesHelper.java
+++ src/main/java/com/netent/Coinking/helper/GameRulesHelper.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class GameRulesHelper {
     public static final Map<Integer, String> rules = ImmutableMap.<Integer, String>builder()
+            .put(0, "NO FREE ROUND")
             .put(1, "FREE ROUND")
             .put(2, "WIN")
             .put(3, "LOST")
